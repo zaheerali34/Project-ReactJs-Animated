c9c9bafc8e90ee6d8aec979afc39a484be11a170
Zaheer Khan <zaheerrahman0@gmail.com>
2024-11-28 22:23:53 +0500

    pagestext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./Components/Home/Home";
+import Pages from "./Components/PagesText/Pages";
 
 function App() {
   return (
     <div>
       <Home />
+      <Pages />
     </div>
   );
 }
---
+++ src/Components/PagesText/Pages.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Pages() {
+  return (
+    <div className='w-full h-full text-center my-4'>
+        <p className='text-xl'>©  anzo.studio 2024 | designed and developed</p>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>impactfui</h1>
+        </div>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>design</h1>
+        </div>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>is the</h1>
+        </div>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>design</h1>
+        </div>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>that</h1>
+        </div>
+
+        <div className='h-[30vw] leading-[40rem] mt-[3rem]'>
+            <h1 className='text-[42vw] font-[file5] uppercase'>works</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Pages
