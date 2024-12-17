807145d56a929cbe5c97f97f3403382c81bab355
Zaheer Khan <zaheerrahman0@gmail.com>
2024-12-17 21:32:01 +0500

    last two

--- src/Components/PageImages/Image.jsx
+++ src/Components/PageImages/Image.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Image() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <img src='../images/Me.avif' alt="" className=" h-full object-cover" />
+      <img src='https://static.wixstatic.com/media/f1c650_50b3eb1a161343a6a87b1fe48cb27f9a~mv2.png/v1/fill/w_595,h_889,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Astro%20Me.png' alt="" className=" h-full object-cover" />
     </div>
   )
 }
